1342d873cbd51558b70b45e31bc76e46a020dcd5
Pratha <Pratha@prathash@usc.edu>
2016-04-25 12:22:58 -0700

    Inital Commit of Messenger API

---
+++ src/main/java/org/pratha/learnrestapi/messenger/Service/MessageService.java
@@ -0,0 +1,13 @@
+package org.pratha.learnrestapi.messenger.Service;
+
+import java.util.List;
+
+import org.pratha.learnrestapi.messenger.Message;
+import org.pratha.learnrestapi.messenger.model.*;
+public class MessageService {
+	public List<Message> getAllMessages()
+	{
+		
+	}
+
+}
